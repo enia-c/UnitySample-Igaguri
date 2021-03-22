8c15041b5a33c70685f53b5dbbc131decf1b3fb5
m-kiyota <masahiro.kiyota@dir.co.jp>
2021-03-22 18:17:50 +0900

    Unityの教科書 Chapter7

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Shoot(new Vector3(0, 200, 2000));
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+}
